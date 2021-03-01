2a0f2d38f077c40f819758aa5fdf65e18f83f9bb
Andre <mae.26@yandex.ru>
2021-03-01 20:55:03 +0700

    sampling_strategy_changed

--- myapp.py
+++ myapp.py
@@ -58,8 +58,8 @@ def get_table_download_link(df):
     """
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
-    #return href
+    href = f'<a href="data:file/csv;base64,{b64}" download="synthetic_data.csv">Download csv file</a>'
+    return href
 
 def main():
     # story ='Я люблю тебя'
