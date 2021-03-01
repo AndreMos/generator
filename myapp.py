6c7763ea2f0fb820b48fd99466fde1ad00cfb35e
Andre <mae.26@yandex.ru>
2021-03-02 03:32:36 +0700

    paths

--- myapp.py
+++ myapp.py
@@ -59,7 +59,6 @@ def reg_columns():
     return st.beta_columns(3)
 
 def main():
-    #os.chdir('/app/generator/')
     dicti_fam = {
     '0' : 'not specified',
     '1' : 'single',
@@ -74,18 +73,13 @@ def main():
     dicti_gen = {'1' : 'female', '2' : 'male'}
     dataset, names, w_names, final_bn, idx_to_interest = load()
 
-    #sn = st.sidebar.text_input('Surname', 'Johnson')
     slider = st.sidebar.selectbox('Age', ['not specified', 'Teen', 'Adult', 'Old'])
-    #slider = st.sidebar.select_slider('Age', ['Child', 'Teen', 'Adult', 'Old'])
     li = ['teen', 'adult', 'old']
     gen = st.sidebar.radio('Gender', ['not specified', 'Male', "Female"])
     but = st.sidebar.button('Generate profiles')
 
     if but:
         age = slider.lower()
-        #gender = gen.lower()
-        #sample
-
         if slider == 'not specified' and  gen == 'not specified':
             bn = sample(final_bn, None, None, names, white_names = w_names)
         elif age == 'not specified':
@@ -95,7 +89,6 @@ def main():
         else:
             bn = sample(final_bn, age = age, gender = gen, names = names, white_names = w_names )
         res = sam(bn)
-        res1 = res
         logging.info("Synt shape: " + str(res.shape))
 
 
@@ -106,28 +99,23 @@ def main():
             fam = dicti_fam[rec['relation'].iloc[0]]
             inter = rec.iloc[0,5:-1].sort_values( ascending = False)[:4].index.values
             gender = dicti_gen[rec['sex'].iloc[0]]
-
-            #inter
-            #inter.sort_values( ascending = False)[:4]
             age1 = rec['age'].iloc[0]
-            #aage
             col2.markdown(f'**Name** : {name}')
             col2.markdown(f'**Age: ** {age1}')
             col2.markdown(f'**Gender: ** {gender}')
             col2.markdown(f'**High education:** {educ}')
             col2.markdown(f'**Family status: ** {fam}')
-            col2.text(' ')
-            col2.text(' ')
-            col2.text(' ')
-            col2.text(' ')
+            # col2.text(' ')
+            # col2.text(' ')
+            # col2.text(' ')
+            # col2.text(' ')
 
             col3.markdown(f"**User interests:** ")
             for k in idx_to_interest[idx_to_interest['topic'].isin(inter)]['key_words'].values:
                 col3.markdown(k)
-            col3.text(' ')
-            col3.text(' ')
+            # col3.text(' ')
+            # col3.text(' ')
 
-            os.chdir('/app/generator/new_generator1')
             h = names[names['name'].str.find(name)!=-1]['type']
             if h.shape[0] > 0:
                 race = h.iloc[0]
@@ -139,16 +127,13 @@ def main():
                 logging.info(ovr.shape)#
                 ovr = ovr.sample(1)
             except:
-                os.chdir('/app/generator/')
                 if fold(age1) == 'teen':
                     ovr = dataset[(dataset['race'] == race) & (dataset['age'] == 'child') & (dataset['gender'] == gender)].sample(1)
-            os.chdir('/app/generator/new_generator1')
             res = ovr['id'].iloc[0]
             path_img = Path(__file__).parent / 'new_generator1'
             avatar = Image.open(path_img / race  /  (str(res) + '.jpg'))
             col1.image(avatar, caption = 'Profile picture')
             col1.text(' ')
-            os.chdir('/app/generator/')
         st.markdown(get_table_download_link(bn), unsafe_allow_html=True)
 
 @st.cache(allow_output_mutation=True)
@@ -157,10 +142,12 @@ def load():
     skel = read_structure('K2_bn_structure')
     params = read_params('K2_bn_param')
     final_bn = HyBayesianNetwork(skel, params)
-    idx_to_interest = pd.read_csv('key_words_groups_interests.csv')
-
-    white_names = pd.read_csv(path / 'data/white_names.csv')
     logging.info('BN downloaded')
+
+    idx_to_interest = pd.read_csv('key_words_groups_interests.csv')
+    white_names = pd.read_csv(path / 'data/white_names.csv')
+
+
     url = 'https://drive.google.com/uc?id=1NQIXdma7J0HJ6WfU0Z7VUcF_ypoJ9UrN'
     output = 'modulus.zip'
     logging.info('Start load')
@@ -169,79 +156,15 @@ def load():
         zip_ref.extractall()
 
     path_f = Path(__file__).parent / 'new_generator1'
-    #stri = '/app/generator/new_generator1/'
     dataset = pd.read_csv(path_f / 'dataset.csv')
     dataset['id'] = dataset['id'].astype(int)
     names = pd.read_csv(path_f / 'names.csv')
 
     return dataset, names, white_names, final_bn, idx_to_interest
 
-    #return Gs
-
-# def generate_images(network_pkl, seeds, truncation_psi):
-#     print('Loading networks from "%s"...' % 'gdrive:networks/stylegan2-ffhq-config-f.pkl')
-#     _G, _D, Gs = pretrained_networks.load_networks('gdrive:networks/stylegan2-ffhq-config-f.pkl')
-#     noise_vars = [var for name, var in Gs.components.synthesis.vars.items() if name.startswith('noise')]
-#
-#     Gs_kwargs = dnnlib.EasyDict()
-#     Gs_kwargs.output_transform = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
-#     Gs_kwargs.randomize_noise = False
-#     if truncation_psi is not None:
-#         Gs_kwargs.truncation_psi = truncation_psi
-#     os.chdir(r'/content/gdrive/My Drive/maga/new_data')
-#     for seed_idx, seed in enumerate(seeds):
-#         print('Generating image for seed %d (%d/%d) ...' % (seed, seed_idx, len(seeds)))
-#         rnd = np.random.RandomState(seed)
-#         z = rnd.randn(1, *Gs.input_shape[1:]) # [minibatch, component]
-#         tflib.set_vars({var: rnd.randn(*var.shape.as_list()) for var in noise_vars}) # [height, width]
-#         images = Gs.run(z, None, **Gs_kwargs) # [minibatch, height, width, channel]
-#         return PIL.Image.fromarray(images[0], 'RGB')
-        #return images
-        #PIL.Image.fromarray(images[0], 'RGB').save(dnnlib.make_run_dir_path('seed%04d.png' % seed))
 
 
-# def process(story):
-#     bert, classif,tokenizer = load_components()
-#     pred_cl=pred(bert, classif,story,tokenizer)
-#     return pred_cl.item()
-#
-#
-# class Net(nn.Module):
-#     def __init__(self, num_feature):
-#         super(Net, self).__init__()
-#         self.layer_1 = nn.Linear(num_feature, 128)
-#         self.layer_2 = nn.Linear(128, 8)
-#         self.relu = nn.ReLU()
-#
-#     def forward(self, x):
-#         x = self.layer_1(x)
-#         x = self.relu(x)
-#         x = self.layer_2(x)
-#         return x
-#
-# @st.cache(allow_output_mutation=True)
-# def load_components():
-#     tokenizer = AutoTokenizer.from_pretrained("DeepPavlov/rubert-base-cased-sentence")
-#     model = AutoModel.from_pretrained("DeepPavlov/rubert-base-cased-sentence")
-#     model_new=Net(768)
-#     model_new.load_state_dict(torch.load('model',map_location=torch.device('cpu')) )
-#     model_new.eval()
-#     return model,model_new,tokenizer
-#
-#
-#
-# def pred(model,model_new,x,tokenizer):
-#     pt_batch = tokenizer(
-#     x,
-#     padding=True,
-#     truncation=True,
-#     return_tensors="pt")
-#
-#     pt_outputs = model(**pt_batch)
-#     out=model_new(pt_outputs[1])
-#     cc=torch.log_softmax(out,dim = 1)
-#     _, y_pred_tags = torch.max(cc, dim = 1)
-#     return y_pred_tags
+
 
 if __name__ == "__main__":
     main()
